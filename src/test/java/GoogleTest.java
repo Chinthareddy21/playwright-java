584fff2305e35c61d14af09150901550a7ff8be2
Bhargava Reddy Chinthareddy <114492951+Chinthareddy21@users.noreply.github.com>
2023-10-31 22:42:02 +0530

    Google test

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,33 @@
+import com.microsoft.playwright.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.testng.annotations.*;
+
+public class GoogleTest {
+
+	private static Page page;
+	private static final Logger log = (Logger) LogManager.getLogger(GoogleTest.class);
+	@BeforeSuite
+	public void setUp() {
+		log.info("Setting up Browser");
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		page = browser.newPage();
+		log.info("Browser started");
+	}
+
+	@AfterSuite
+	public void tearDown(){
+		page.close();
+		log.info("Closing Browser");
+	}
+
+	@Test
+	public void google() {
+		page.navigate("https://www.google.co.in/");
+		log.info("Navigated to Google search page");
+		page.locator(".gLFyf").fill("Mountains");
+		log.info("Entered Mountains in search box");
+		page.locator(".gLFyf").press("Enter");
+		log.info("Clicking on enter to search");
+	}
+}
